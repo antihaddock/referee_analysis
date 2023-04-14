ba69690590458c750e1c4a8b439c7510e2ceea07
antihaddock <antihaddock1@gmail.com>
2023-04-14 16:48:07 +1000

    BQ and dbt setup

---
+++ prefect/ingest_to_bigquery.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+import pandas_gbq
+import os
+
+
+# @task(retries=3)
+# def extract_from_gcs() -> Path:
+#     """Download data from GCS to facilitate uploading into BigQuery"""
+#     gcs_path = "data/"
+#     gcs_block = GcsBucket.load("engineering-camp")
+#     local_path = './data_engineering_camp_project/'
+#     gcs_block.get_directory(from_path=gcs_path, local_path=local_path)
+#     saved_path = f'{local_path}/{gcs_path}'
+#     return Path(saved_path)
+
+def get_files(path):
+    """ A function to find all files in a directory and save the path into a list""" 
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            files.append(os.path.join(r, file))
+    return files
+
+
+
+@task(log_prints=True)
+def write_to_bq() -> None:
+    """Writes a DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("referee-credentials")        
+   
+    files = get_files('/home/antihaddock/Repos/ref_analysis/data/')
+    for file in files:
+        # take the path of the file and return just its name without spaces to come up 
+        # with the Big Query Table name
+        bq_table_name = os.path.splitext(os.path.basename(file))[0].replace(" ", "")   
+       
+        if file.endswith('.csv'):
+            df = pd.read_csv(file, low_memory=False)
+        elif file.endswith('.xlsx'):
+            df = pd.read_excel(file)
+        else:
+            print(f"{file} is not a CSV or XLSX file.")
+
+        # Pre processing of data   
+        df = df.dropna()    
+        # Define a dictionary that maps the old column names to the new ones
+        column_mapping = {
+                      "Item/Acct" : 'Position',
+                      "AUTO PLUS + (ADDITIONAL MATCHES)": "Suspension",
+                      "PLAYER/TEAM OFFICIAL": "PLAYEROROFFICIAL"
+                     }           
+
+        # Loop through each old column name and its corresponding new column name
+        for old_col, new_col in column_mapping.items():
+        # If the old column exists in the DataFrame
+            if old_col in df.columns:
+                df.rename(columns={old_col: new_col}, inplace=True)
+        
+        # overcome issues with columns not being in the right format and case everything to bytes        
+        for col in df.columns:
+                df[col] = df[col].astype(bytes)
+                        
+        ## Upload file to BQ     
+        table_name = f"appointments_data_raw.{bq_table_name}"
+        print(f' table name is: {table_name}')
+        
+        df.to_gbq(
+            destination_table = table_name,
+            project_id="refereeinganalysis",
+            credentials=gcp_credentials_block.get_credentials_from_service_account(),
+            if_exists='replace'
+            )
+
+@flow()
+def etl_gcs_to_bq() -> None:
+    """Main ETL flow to load data into Big Query"""
+    write_to_bq()
+
+if __name__ == "__main__":
+     etl_gcs_to_bq()
--- prefect/ingest_to_gcs_bucket.py
+++ prefect/ingest_to_gcs_bucket.py
@@ -1,21 +1,34 @@
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
+import os
+
+def get_files(path):
+    """ A function to find all files in a directory and save the path into a list""" 
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            files.append(os.path.join(r, file))
+    return files
+
 
 @task()
 def write_gcs(bucket: str) -> None:
-    """Upload local csv file to GCS"""
+    """Upload files from a directory into GCS"""
     gcs_block = GcsBucket.load(bucket)
-    path = '/home/antihaddock/Repos/data_engineering_camp_project/data/research_payments.csv'
-    gcs_path = '/data/'
-    gcs_block.upload_from_path(from_path=path, to_path=gcs_path, timeout=None)
+    path = '/home/antihaddock/Repos/ref_analysis/data/'
+    files = get_files(path)
+    # Loop over all files in the directory and subdirectory and upload to GCP
+    for file in files:
+         # Get a specific GCS path to store the file
+        gcs_path = file.split('data/')[1]
+        gcs_block.upload_from_path(from_path=file, to_path=gcs_path, timeout=None)
     return
 
 
 @flow()
 def etl_web_to_gcs() -> None:
     """Upload data into a GCS bucket"""
-    gcs_bucket_name = "engineering-camp"
+    gcs_bucket_name = "refereeing-bucket"
     write_gcs(gcs_bucket_name)
 
 
